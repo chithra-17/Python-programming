5cb9c68c8148726dac5a517b2203462a571967a9
chithra-17 <chithsd144@gmail.com>
2026-03-10 19:38:32 +0530

    Added Polymorphism program

---
+++ polymorphism.py
@@ -0,0 +1,17 @@
+# Polymorphism
+
+class dog:
+    def sound(self):
+        print("dog barks")
+
+class cat:
+    def sound(Self):
+        print("cat meows")
+
+d = dog()
+c = cat()
+
+d.sound()
+c.sound()
+
+    
