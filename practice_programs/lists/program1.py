5c5301b18ff1e93662f58e3d9f8a948154907400
chithra-17 <chithsd144@gmail.com>
2026-03-13 16:12:20 +0530

    Added new practiced programs

---
+++ practice_programs/lists/program1.py
@@ -0,0 +1,19 @@
+names=["ali" , "sara" , "aman"]
+for i in names:
+    if i.endswith("a"):
+        print("names:",i)
+
+
+
+name = ["rani","raju","sara"]
+for i in name:
+    if i.startswith("r"):
+        print("name:",i)
+
+
+l=["cow","horse","moon"]
+i=0
+
+while(i<len(l)):
+    print(l[i])
+    i+=1
