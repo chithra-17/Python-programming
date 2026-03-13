cde9a1bab5a2e28874f77ccef1eb995397fd049c
chithra-17 <chithsd144@gmail.com>
2026-03-13 23:20:22 +0530

    Added practiced program

---
+++ practice_programs/strings/program3.py
@@ -0,0 +1,16 @@
+food="pasta"
+print(food.replace('a','n',1))# replace(old,new,count)
+
+
+
+king="bahubali"
+print(king and "Mahishmati") # ex: A and B : if a is false =return A  and if A is true = return B
+#here king is non empty string so it "True" return "Mahishmati"
+
+
+
+s="king"
+
+for i in s:
+    print(i * 2, end=" ") #"" == no space , " " == space;
+    #print(i * 2, end="")
