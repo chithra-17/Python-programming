ce8606d7a8bda4990e532ee1a9d11bd2d2744a4c
chithra-17 <chithsd144@gmail.com>
2026-08-09 11:29:53 +0530

    Added Attribute Numpy Program

---
+++ Numpy/program3.py
@@ -0,0 +1,11 @@
+#Array Attributes
+import numpy as np
+arr=np.array([[10,20,30],
+             [40,50,60]])
+
+print(arr.shape)
+print(arr.ndim)
+print(arr.size)
+print(arr.dtype)
+print(arr.itemsize)
+print(arr.nbytes)
